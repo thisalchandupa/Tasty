af2da93b177dbb506e71e0191b7061df672b8c8c
thisalchandupa <146944086+thisalchandupa@users.noreply.github.com>
2024-06-05 11:17:15 +0530

    test1

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore Our Menu</h1>
-      <p className='explore-menu-test'>Choose from this</p>
+      <p className='explore-menu-test'>Gay</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
           return(
