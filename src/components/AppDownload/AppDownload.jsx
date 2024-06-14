c6d78c2d292f54afe78b39ef2f99ac65131d58d8
thisalchandupa <146944086+thisalchandupa@users.noreply.github.com>
2024-06-14 17:24:08 +0530

    App download comp

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+        <p>Download the <br />Tasty App</p>
+        <div className="app-downlooad-platforms">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default AppDownload
